04c9f4adbfd72c91e45da05d40bbb62eadf79d46
Vinícius Manjabosco <tallibba@gmail.com>
2024-05-15 15:26:39 +0200

    Add solution for E 26 Remove Duplicates from Sorted Array

---
+++ src/26 - Remove Duplicates from Sorted Array.ts
@@ -0,0 +1,12 @@
+export {}; // Necessary in order to avoid TS errors
+
+function removeDuplicates(nums: number[]): number {
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+
+  return nums.length;
+}
