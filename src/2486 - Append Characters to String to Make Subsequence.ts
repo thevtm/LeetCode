81dd9ff7aac25290131ef1ba563974199379a850
Vinícius Manjabosco <tallibba@gmail.com>
2024-06-03 10:38:00 +0200

    Add solution for M 2486 Append Characters to String to Make Subsequence

---
+++ src/2486 - Append Characters to String to Make Subsequence.ts
@@ -0,0 +1,13 @@
+export {}; // Necessary in order to avoid TS errors
+
+function appendCharacters(s: string, t: string): number {
+  let j = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t[j]) {
+      j++;
+    }
+  }
+
+  return t.length - j;
+}
