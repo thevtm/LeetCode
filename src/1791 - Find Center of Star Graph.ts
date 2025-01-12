8a866a9f609b42d859c187ea2f81eaf128f80ca2
Vinicius T. Manjabosco <978715+thevtm@users.noreply.github.com>
2025-01-12 08:47:14 +0100

    Add solution for E 1791 Find Center of Star Graph

---
+++ src/1791 - Find Center of Star Graph.ts
@@ -0,0 +1,9 @@
+export {};
+
+function findCenter(edges: number[][]): number {
+  if (edges[0][0] === edges[1][0] || edges[0][0] === edges[1][1]) {
+    return edges[0][0];
+  } else {
+    return edges[0][1];
+  }
+}
