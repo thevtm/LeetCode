b52a22772b8b71addef6a23e34072b903195d7cf
Vinicius T. Manjabosco <978715+thevtm@users.noreply.github.com>
2025-01-17 17:44:40 +0100

    Moved solutions to the correct directory

--- src/1863 - Sum of All Subset XOR Totals.ts
+++ src/1863 - Sum of All Subset XOR Totals.ts
@@ -1,21 +1,25 @@
-export {}; // Necessary in order to avoid TS errors
+export {};
 
 function subsetXORSum(nums: number[]): number {
-  const acc: number[] = [];
-  let result: number = 0;
+  let xor_total = 0;
+  let xor_acc = 0;
 
   const backtrack = (index: number = 0): void => {
-    result += acc.reduce((a, x) => a ^ x, 0);
+    const xor_acc_before = xor_acc;
 
     for (let i = index; i < nums.length; i++) {
       const n = nums[i];
-      acc.push(n);
+
+      xor_acc ^= n;
+      xor_total += xor_acc;
+
       backtrack(i + 1);
-      acc.pop();
+
+      xor_acc = xor_acc_before;
     }
   };
 
   backtrack();
 
-  return result;
+  return xor_total;
 }
