057b23e517fcc8776841bde0e04468b9b2c2c114
Vinicius T. Manjabosco <978715+thevtm@users.noreply.github.com>
2025-03-19 00:58:02 +0100

    Refactor solution for E 69 Sqrt(x)

--- src/69 - Sqrt(x).ts
+++ src/69 - Sqrt(x).ts
@@ -1,11 +1,23 @@
 export {}; // Necessary in order to avoid TS errors
 
 function mySqrt(x: number): number {
-  let i = 1;
+  let lower = 0;
+  let upper = x;
+  let result = 0;
 
-  while (i * i <= x) {
-    i++;
+  while (lower <= upper) {
+    const middle = Math.floor((upper + lower) / 2);
+    const middle_square = middle * middle;
+
+    if (middle_square === x) {
+      return middle;
+    } else if (middle_square < x) {
+      result = Math.max(result, middle);
+      lower = middle + 1;
+    } else {
+      upper = middle - 1;
+    }
   }
 
-  return i - 1;
+  return result;
 }
